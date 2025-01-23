6072d9882d5121fdc39acc7a6390f80cfa6c7cf0
Dip Patel <pateldeep2332@gmail.com>
2025-01-23 13:11:32 +0530

    Implemented events for playermovement

--- Array-Jumper/header/Player/PlayerController.h
+++ Array-Jumper/header/Player/PlayerController.h
@@ -1,18 +1,23 @@
 #pragma once
+#include "../Event/EventService.h"
 
 namespace Player
 {
 	class PlayerView;
 	class PlayerModel;
 	enum class PlayerState;
+	enum MovementDirection;
 
 	class PlayerController
 	{
 	private:
 		PlayerModel* player_model;
 		PlayerView* player_view;
+		EventM::EventService* event_service;
 
 		void destroy();
+		bool isPositionInBound(int targetPosition);
+		void move(MovementDirection direction);
 
 	public:
 		PlayerController();
@@ -22,6 +27,7 @@ namespace Player
 		void update();
 		void render();
 
+		void readInput();
 		PlayerState getPlayerState();
 		void setPlayerState(PlayerState new_player_state);
 
